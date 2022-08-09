792bc1d31e144b464810df85810eab66fe71d784
dshreyashi <shreyashidabral543@gmail.com>
2022-08-09 23:37:03 +0530

    Level3 Authentication - Hashing Passwords

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,13 +24,9 @@ const userSchema = new mongoose.Schema({
 
 //Plugins : extended bits of packaged code added to mongoose schema to give them more functionality
 //fetching secretkey from .env file
-userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ['password'] });
+// userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ['password'] });
 
 const User = new mongoose.model("User", userSchema);
-
-
-
-
 
 
 // ****** Get and Post requests ********
@@ -45,7 +42,7 @@ app.route("/register")
     .post(function(req, res){
         const newUser = new User({
             email : req.body.username,
-            password : req.body.password
+            password : md5(req.body.password)
         })
         newUser.save(function(err){
             if(!err){
@@ -62,7 +59,7 @@ app.route("/login")
     })
     .post(function(req, res){
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
 
         User.findOne({email : username}, function(err, foundUser){
             if(err){
@@ -76,8 +73,6 @@ app.route("/login")
             }
         });
     });
-
-
 
 app.listen(3000, function(){
     console.log("Server started successfully.");
